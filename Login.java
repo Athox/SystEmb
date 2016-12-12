089a44b5f4ecde2badfc3b7d97744e5c9cda6230
Athox <cot.anthony@gmail.com>
2016-12-12 15:43:15 +0100

    import

---
+++ Login.java
@@ -0,0 +1,29 @@
+package com.example.acha_ou_teung.systemb;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    Button button_inscription, button_connexion;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button_inscription = (Button)findViewById(R.id.button_inscription);
+        button_inscription.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+
+    }
+}
